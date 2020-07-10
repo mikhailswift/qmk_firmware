5e9f7e120c8c35adaff5a28e854a4d64ab68f5d9
Mikhail Swift <mswift@mswift.dev>
2020-07-10 16:24:11 -0400

    rework brace tap dances

--- keyboards/ergodox_ez/keymaps/mikhailswift/tapdances.h
+++ keyboards/ergodox_ez/keymaps/mikhailswift/tapdances.h
@@ -3,61 +3,53 @@
 enum tap_dances {
   TD_LBRC_COMBO,
   TD_RBRC_COMBO,
-  TD_PRN,
-  TD_BRC,
-  TD_BKT,
 };
 
-void dance_lbrc_combo_fin(qk_tap_dance_state_t *state, void *user_data) {
-    if (state->count == 1) {
-        register_code(KC_RSFT);
-        register_code(KC_9);
-    } else if (state->count == 2) {
-        register_code(KC_RSFT);
-        register_code(KC_LBRC);
-    } else {
-        register_code(KC_LBRC);
+void dance_brc_combo_fin(qk_tap_dance_state_t *state, void *user_data) {
+  if (state->count == 1) {
+    if (state->keycode == TD(TD_LBRC_COMBO)) {
+      register_code16(KC_LPRN);
+    } else if (state->keycode == TD(TD_RBRC_COMBO)) {
+      register_code16(KC_RPRN);
     }
+  } else if (state->count == 2) {
+    if (state->keycode == TD(TD_LBRC_COMBO)) {
+      register_code16(KC_LCBR);
+    } else if (state->keycode == TD(TD_RBRC_COMBO)) {
+      register_code16(KC_RCBR);
+    }
+  } else {
+    if (state->keycode == TD(TD_LBRC_COMBO)) {
+      register_code(KC_LBRC);
+    } else if (state->keycode == TD(TD_RBRC_COMBO)) {
+      register_code(KC_RBRC);
+    }
+  }
 }
 
-void dance_lbrc_combo_reset(qk_tap_dance_state_t *state, void *user_data) {
-    if (state->count == 1) {
-        unregister_code(KC_RSFT);
-        unregister_code(KC_9);
-    } else if (state->count == 2) {
-        unregister_code(KC_RSFT);
-        unregister_code(KC_LBRC);
-    } else {
-        unregister_code(KC_LBRC);
+void dance_brc_combo_reset(qk_tap_dance_state_t *state, void *user_data) {
+  if (state->count == 1) {
+    if (state->keycode == TD(TD_LBRC_COMBO)) {
+      unregister_code16(KC_LPRN);
+    } else if (state->keycode == TD(TD_RBRC_COMBO)) {
+      unregister_code16(KC_RPRN);
     }
-}
-
-void dance_rbrc_combo_fin(qk_tap_dance_state_t *state, void *user_data) {
-    if (state->count == 1) {
-        register_code(KC_RSFT);
-        register_code(KC_0);
-    } else if (state->count == 2) {
-        register_code(KC_RSFT);
-        register_code(KC_RBRC);
-    } else {
-        register_code(KC_RBRC);
+  } else if (state->count == 2) {
+    if (state->keycode == TD(TD_LBRC_COMBO)) {
+      unregister_code16(KC_LCBR);
+    } else if (state->keycode == TD(TD_RBRC_COMBO)) {
+      unregister_code16(KC_RCBR);
     }
-
-}
-
-void dance_rbrc_combo_reset(qk_tap_dance_state_t *state, void *user_data) {
-    if (state->count == 1) {
-        unregister_code(KC_RSFT);
-        unregister_code(KC_0);
-    } else if (state->count == 2) {
-        unregister_code(KC_RSFT);
-        unregister_code(KC_RBRC);
-    } else {
-        unregister_code(KC_RBRC);
+  } else if (state->count > 2) {
+    if (state->keycode == TD(TD_LBRC_COMBO)) {
+      unregister_code(KC_LBRC);
+    } else if (state->keycode == TD(TD_RBRC_COMBO)) {
+      unregister_code(KC_RBRC);
     }
+  }
 }
 
 qk_tap_dance_action_t tap_dance_actions[] = {
-    [TD_LBRC_COMBO] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_lbrc_combo_fin, dance_lbrc_combo_reset),
-    [TD_RBRC_COMBO] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_rbrc_combo_fin, dance_rbrc_combo_reset),
+  [TD_LBRC_COMBO] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_brc_combo_fin, dance_brc_combo_reset),
+  [TD_RBRC_COMBO] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_brc_combo_fin, dance_brc_combo_reset),
 };
