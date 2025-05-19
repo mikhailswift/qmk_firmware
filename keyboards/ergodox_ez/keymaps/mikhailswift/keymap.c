aaa8c892ef42e3355a175e3c4d2a341d5d29a23c
Mikhail Swift <mikhail@testifysec.com>
2025-05-19 13:44:38 -0400

    chore: update keymap with new keycodes and built-in tap dance macro

--- keyboards/ergodox_ez/keymaps/mikhailswift/keymap.c
+++ keyboards/ergodox_ez/keymaps/mikhailswift/keymap.c
@@ -10,7 +10,7 @@
 #define KC_VOL_UP  KC_AUDIO_VOL_UP
 #define KC_MUTE KC_AUDIO_MUTE
 #define KC_SFT_SPACE LSFT_T(KC_SPACE)
-#define KC_CTL_BSPACE LCTL_T(KC_BSPACE)
+#define KC_CTL_BSPACE LCTL_T(KC_BSPC)
 #define KC_ALT_TAB LALT_T(KC_TAB)
 #define KC_SFT_ENTER LSFT_T(KC_ENTER)
 #define KC_TDLBRC TD(TD_LBRC_COMBO)
@@ -37,22 +37,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [QWERTY] = LAYOUT_ergodox_pretty(
     KC_TRANSPARENT, KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_TRANSPARENT,
     LGUI(KC_TAB),   KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_TDLBRC,                                      KC_TDRBRC,      KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_TRANSPARENT,
-    LGUI_T(KC_ESC), KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_QUOTE,
+    LGUI_T(KC_ESC), KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        KC_QUOTE,
     OSL(SYMBOLS),   KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_MINUS,                                       KC_EQUAL,       KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       OSL(ADJUST),
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_F5,                                                                                                          KC_DEL,         KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-                                                                                                    LCTL(KC_LALT),  KC_SCROLLLOCK,  KC_PAUSE,       KC_PSCR,
+                                                                                                    LCTL(KC_LALT),  KC_SCRL,        KC_PAUSE,       KC_PSCR,
                                                                                                                     KC_HOME,        KC_PGUP,
-                                                                                    KC_SFT_SPACE,   KC_CTL_BSPACE,  KC_END,         KC_PGDOWN,      KC_ALT_TAB, KC_SFT_ENTER 
+                                                                                    KC_SFT_SPACE,   KC_CTL_BSPACE,  KC_END,         KC_PGDN,        KC_ALT_TAB, KC_SFT_ENTER
   ),
   [GAMING] = LAYOUT_ergodox_pretty(
     KC_ESC,         KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
     KC_TAB,         KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_LSHIFT,      KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_LCTRL,       KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+    KC_LSFT,        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+    KC_LCTL,        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
                                                                                                     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
                                                                                                                     KC_TRANSPARENT, KC_TRANSPARENT,
-                                                                                          KC_SPACE, KC_BSPACE,      KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
+                                                                                          KC_SPACE, KC_BSPC,        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
   ),
   [SYMBOLS] = LAYOUT_ergodox_pretty(
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
@@ -68,8 +68,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRANSPARENT, KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          RGB_TOG,                                        KC_TRANSPARENT, KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_MS_UP,       KC_TRANSPARENT, KC_MS_WH_UP,    RGB_VAI,                                        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_F12,
     KC_TRANSPARENT, KC_TRANSPARENT, KC_MS_LEFT,     KC_MS_DOWN,     KC_MS_RIGHT,    KC_MS_WH_DOWN,                                                                  KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, RGB_VAD,                                        KC_NUMLOCK,     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    RESET,          KC_TRANSPARENT, KC_TRANSPARENT, KC_MS_BTN1,     KC_MS_BTN2,                                                                                                     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, TG(GAMING),
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, RGB_VAD,                                        KC_NUM,         KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+    QK_BOOT,        KC_TRANSPARENT, KC_TRANSPARENT, KC_MS_BTN1,     KC_MS_BTN2,                                                                                                     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, TG(GAMING),
                                                                                                     KC_TRANSPARENT, KC_MUTE,        KC_TRANSPARENT, KC_TRANSPARENT,
                                                                                                                     KC_VOL_UP,      KC_NEXT,
                                                                                     KC_TRANSPARENT, KC_TRANSPARENT, KC_VOL_DOWN,    KC_PREV,        KC_TRANSPARENT, KC_TRANSPARENT
--- keyboards/ergodox_ez/keymaps/mikhailswift/tapdances.h
+++ keyboards/ergodox_ez/keymaps/mikhailswift/tapdances.h
@@ -5,7 +5,13 @@ enum tap_dances {
   TD_RBRC_COMBO,
 };
 
-void dance_brc_combo_fin(qk_tap_dance_state_t *state, void *user_data) {
+tap_dance_action_t tap_dance_actions[] = {
+    [TD_LBRC_COMBO] = ACTION_TAP_DANCE_DOUBLE(KC_LBRC, KC_LPRN),
+    [TD_RBRC_COMBO] = ACTION_TAP_DANCE_DOUBLE(KC_RBRC, KC_RPRN),
+};
+
+/*
+void dance_brc_combo_fin(tap_dance_state_t *state, void *user_data) {
   if (state->count == 1) {
     if (state->keycode == TD(TD_LBRC_COMBO)) {
       register_code16(KC_LBRC);
@@ -21,7 +27,7 @@ void dance_brc_combo_fin(qk_tap_dance_state_t *state, void *user_data) {
   }
 }
 
-void dance_brc_combo_reset(qk_tap_dance_state_t *state, void *user_data) {
+void dance_brc_combo_reset(tap_dance_state_t *state, void *user_data) {
   if (state->count == 1) {
     if (state->keycode == TD(TD_LBRC_COMBO)) {
       unregister_code16(KC_LBRC);
@@ -37,7 +43,8 @@ void dance_brc_combo_reset(qk_tap_dance_state_t *state, void *user_data) {
   }
 }
 
-qk_tap_dance_action_t tap_dance_actions[] = {
+tap_dance_action_t tap_dance_actions[] = {
   [TD_LBRC_COMBO] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_brc_combo_fin, dance_brc_combo_reset),
   [TD_RBRC_COMBO] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_brc_combo_fin, dance_brc_combo_reset),
 };
+*/
